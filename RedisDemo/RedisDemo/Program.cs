41c65e5cc966206753cb5f6bfb90bd0fbce7ae1f
venux <luwenbiao333@gmail.com>
2017-01-13 14:18:57 +0800

    添加RedisDemo

---
+++ RedisDemo/RedisDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer connection = RedisHelper.Connection;
+
+            bool isConnected = connection != null;
+            Console.WriteLine("是否连接成功：{0}", isConnected == true ? "是" : "否");
+
+            IDatabase database = connection.GetDatabase();
+            
+            if (database.StringSet("testkey1", "testvalue1", new TimeSpan(1000), When.NotExists, CommandFlags.None))
+                Console.WriteLine("已成功添加键！");
+
+            string value = database.StringGet("tesetkey1");
+            Console.WriteLine("键testkey1的值为：" + value);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ RedisDemo/RedisDemo/RedisHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisDemo
+{
+    internal class RedisHelper
+    {
+        public RedisHelper()
+        {
+
+        }
+
+        /// <summary>
+        /// Redis链接
+        /// </summary>
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                try
+                {
+                    return lazyConnection.Value;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            ConfigurationOptions options = new ConfigurationOptions();
+            options.ClientName = "Client1";
+            options.ServiceName = "localhost";
+            //options.Password = System.Configuration.ConfigurationManager.AppSettings["RedisPassword"];
+            options.AbortOnConnectFail = false;
+            options.SyncTimeout = 5000;
+            options.EndPoints.Add("myreids");
+
+            return ConnectionMultiplexer.Connect(options);
+        });
+    }
+}
